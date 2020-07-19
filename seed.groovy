438cf455beadbf8cd6e8d02721ea0244f25c01d7
Premchand <Premchandg278@gmail.com>
2020-07-19 17:12:00 +0530

    Create seed.groovy

---
+++ seed.groovy
@@ -0,0 +1,17 @@
+job("deploy on k8s") {
+	description()
+	keepDependencies(false)
+	disabled(false)
+	concurrentBuild(false)
+	steps {
+		shell("""rm  -rvf  /root/files
+mkdir  /root/files/
+git  clone  https://github.com/Premchandg278/Devops12.git /root/files/
+cp  /root/files/*   /terraform/
+cd /terraform/
+terraform init
+terraform apply -auto-approve
+terraform destroy -auto-approve
+kubectl apply -k .""")
+	}
+}
